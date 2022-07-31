39358745a0a762f0b20ae5ad21802d01999205e2
Shreya <shreya.maity02@gmail.com>
2022-07-31 13:26:16 +0530

    solution added

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let len=word.length;
+    let str='';
+    for(let i=len-1;i>=0;i--){
+        str+=word.substr(i,1);
+    }
+    return str;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        //console.log(item);
+        if (!args.includes(item)) {
+            //console.log(args);
+            newArray.push(item);
+}
+});
+return newArray;
 };
+
+//remove([1,2,3,4],3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
